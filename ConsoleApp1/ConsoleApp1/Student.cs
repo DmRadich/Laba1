1b99884feb712fe91dd39d0299008205d488f7bc
DmRadich <95radich95@gmail.com>
2020-03-24 17:06:29 +0300

    3

---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public string Name { get; set; }
+
+        public int Kurs { get; set; }
+
+        public string Gender { get; set; }
+    
+        public void add()
+        {
+            Name = Console.ReadLine();
+            Kurs = Convert.ToInt32(Console.ReadLine());
+            Gender = Console.ReadLine();
+        }
+        public void Watch()
+        {
+
+            Console.Write(" " + Name);
+            Console.Write(" " + Kurs);
+            Console.Write(" " + Gender);
+        }       
+    }
+}
